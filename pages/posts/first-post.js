1f629c15d3016e86e6c3adee7ddd3f4419720712
FrancescoSciab <francesco.sciabbarrasii@gmail.com>
2023-05-29 18:24:55 +0100

    styling ok

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -9,20 +9,6 @@ export default function FirstPost() {
         <Head>
             <title>First Post</title>
         </Head>
-
-        <Script 
-            src="https://connect.facebook.net/en_US/sdk.js"
-            strategy="lazyOnload" //strategy controls when the third-party script should load. A value of lazyOnload tells Next.js to load this particular script lazily during browser idle time
-            onLoad={() => // onLoad is used to run any JavaScript code immediately after the script has finished loading. In this example, we log a message to the console that mentions that the script has loaded correctly
-                console.log('script loaded correctly, window.FB has been populated')}
-        />
-
-        <h1>First Post</h1>
-
-        <h2>
-            <Link href="/">Back to Home</Link>
-        </h2>
-
     </Layout>
     )
 }
